967118f6105fd20f0871ad0263451f7ada0b5f0a
Tertius Roach <tertius.roach@gmail.com>
2023-12-31 16:32:07 +0200

    Cleaned Clutter: workflow-setup_2.01

--- src/front-end/pages/index/index.ts
+++ src/front-end/pages/index/index.ts
@@ -1,4 +1,12 @@
-export namespace Index {
+// export namespace Index {
+//   // new GetDesign.forPage('default-body'); //
+//   // new GetDesign.forPage('login-body');
+//   console.log(`🠊 ${'index'}.js Loaded 🠈`);
+// }
+
+import { GetSection } from '../../utilities/GetSection';
+
+namespace Index {
   // new GetDesign.forPage('default-body'); //
   // new GetDesign.forPage('login-body');
   console.log(`🠊 ${'index'}.js Loaded 🠈`);
--- src/front-end/utilities/GetSection.ts
+++ src/front-end/utilities/GetSection.ts
@@ -1,7 +1,7 @@
 export namespace GetSection {
   export class forSection {
     constructor(blockName: String) {
-      console.log('GetSection Loaded');
+      console.log(`🠊 GetSection.js Loaded 🠈`);
     }
   }
 }
